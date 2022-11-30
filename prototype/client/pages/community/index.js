13973965b742e607ef19f84486a1a356893f7cf7
chrischo <chrischo@bu.edu>
2022-11-30 17:51:57 -0500

    update

--- prototype/client/pages/community/index.js
+++ prototype/client/pages/community/index.js
@@ -1,9 +1,12 @@
-const { Heading } = require("@chakra-ui/react");
+import { Heading, Input, Box } from "@chakra-ui/react";
 
 const CommunityPage = () => {
-    return(
-        <Heading>Join a community</Heading>
-    )
-}
+  return (
+    <Box>
+      <Heading>Join Community</Heading>
+      <Input placeholder="Search For Community" variant="filled" />
+    </Box>
+  );
+};
 
 export default CommunityPage;
--- prototype/client/pages/index.js
+++ prototype/client/pages/index.js
@@ -1,38 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-import { collection, query, where, getDocs, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
 import { firestore } from "../lib/firebase";
 import { useState, useEffect } from "react";
 
-import { Heading } from '@chakra-ui/react'
-import CommunityList from '../components/CommunityList'
+import { Heading, Box, Center } from "@chakra-ui/react";
+import CommunityList from "../components/CommunityList";
 
 export default function Home(props) {
   return (
-    <div>
-      <Heading>Hello World</Heading>
+    <Center>
       <CommunityList communities={props.communities}></CommunityList>
-    </div>
-  )
+    </Center>
+  );
 }
 
 export async function getServerSideProps(context) {
-
   const communities = [];
 
-  const docRef = collection(firestore, "communities")
-  const querySnapshot = await getDocs(docRef)
+  const docRef = collection(firestore, "communities");
+  const querySnapshot = await getDocs(docRef);
 
   querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      communities.push(doc.id)
-    });
+    // doc.data() is never undefined for query doc snapshots
+    communities.push(doc.id);
+  });
 
   return {
-      props: { 
-        communities: communities,
+    props: {
+      communities: communities,
     }, // will be passed to the page component as props
   };
 }
